9b4b4b290f2a3ba85fbf2ade8f78c7beee2a27f0
Mounir <zaghba.mounir@gmai.com>
2018-09-21 19:19:55 +0200

    update algorithms

--- big_data_dermato2.py
+++ big_data_dermato2.py
@@ -9,23 +9,24 @@ spark = SparkSession(sc)
 # Load and parse the data file.
 #data = MLUtils.loadLibSVMFile(sc, "/home/user/Bureau/spark/projet1/bigdatadermato.csv")
 
-df = spark.read.format("csv").option("header", "true").option("sep",",").option("inferSchema","true").load("/home/user/Bureau/spark/projet1/bigdatadermato.csv")
+df = spark.read.format("csv").option("header", "true").option("sep",";").option("inferSchema","true").load("/home/user/Bureau/spark/projet1/bigdatadermato.csv")
 #df.show(10)
 
 from pyspark.ml.feature import StringIndexer
 disieseIndexer=StringIndexer(inputCol="Classe",outputCol="disieseIndex")
 from pyspark.ml.feature import VectorAssembler
-featureColumns='\"V1\"'
-for i in range(2,225):
-	featureColumns=featureColumns+','+'\"V'+str(i)+'\"'
-
-vectorAssembler=VectorAssembler(inputCols=[featureColumns],outputCol="features")
+#featureColumns='\"V1\"'
+#for i in range(2,225):
+#	featureColumns=featureColumns+','+'\"V'+str(i)+'\"'
+features =df.schema.names[1:225]
+vectorAssembler=VectorAssembler(inputCols=features,outputCol="features")
 data=vectorAssembler.transform(df)
 index_model=disieseIndexer.fit(data)
 data_indexed=index_model.transform(data)
 
 
 trainingData, testData = data_indexed.randomSplit([0.8, 0.2],0.0)
+# 1- Decision Tree
 
 from pyspark.ml.classification import DecisionTreeClassifier
 dt=DecisionTreeClassifier().setLabelCol("disieseIndex").setFeaturesCol("features")
@@ -33,8 +34,21 @@ model = dt.fit(trainingData)
 classifications =model.transform(testData)
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 evaluator=MulticlassClassificationEvaluator(labelCol="disieseIndex",predictionCol="prediction", metricName="accuracy")
-accuracy = evaluator.evaluate(classifications)
+accuracyDecisionTree = evaluator.evaluate(classifications)
+
+
+# 2- Random Forest
+
+from pyspark.ml.classification import RandomForestClassifier
+rf =RandomForestClassifier().setLabelCol("disieseIndex").setFeaturesCol("features").setNumTrees(100)
+modelRF = rf.fit(trainingData)
+classificationsRF = modelRF.transform(testData)
+accuracyRF = evaluator.evaluate(classificationsRF)
+
 print("********************************************************************************")
-print("Test set accuracy = " + str(accuracy))
+print("Test set accuracy Decision Tree= " + str(accuracyDecisionTree))
 print("********************************************************************************")
 
+print("********************************************************************************")
+print("Test set accuracy RF= " + str(accuracyRF))
+print("********************************************************************************")
